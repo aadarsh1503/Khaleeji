04b1ac3b9bdcefb1acdc683a94a79ccd31b4576e
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-01-02 14:31:24 +0530

    done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import i1 from "./i1.jpg"
+import i1 from "./i1.png"
 const Navbar = () => {
   return (
-    <nav className="bg-dblack  p-2 z-0 px-6 shadow-md">
+    <nav className="bg-dblack   shadow-md">
       <div className="flex items-center max-w-7xl mx-auto justify-between">
         {/* Logo */}
         <div className="text-xl font-semibold text-gray-800">
           <img
             src={i1}
             alt="Logo"
-            className="h-16"
+            className="h-24"
           />
         </div>
       </div>
